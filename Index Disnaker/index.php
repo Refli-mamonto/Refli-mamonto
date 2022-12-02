3e53e0f98afed8b5ea2fbae60f4fdf344d110006
Refli-mamonto <119617366+Refli-mamonto@users.noreply.github.com>
2022-12-03 05:47:55 +0800

    Add files via upload
    Halaman Index, Style Css Dan Image Source

---
+++ Index Disnaker/index.php
@@ -0,0 +1,434 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perlindungan Dan Pengawasan Tenaga Kerja</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="style/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header" style="background-color: #1A2238;">
+
+    <a href="#" class="logo" style="color:white;"> <i class="fas fa-users"></i>Menu Utama</a>
+
+    <nav class="navbar">
+        <a href="#home" style="color:white">home</a>
+        <a href="#services"style="color:white">Petunjuk</a>
+        <a href="#about"style="color:white">Tentang Kami</a>
+        <a href="#doctors"style="color:white">galeri</a>
+        <!-- <a href="#book">Register</a> -->
+        <a href="#review"style="color:white">review</a>
+       
+    </nav>
+ 	<a class="btn" style="cursor:pointer;color:black;" onclick="login()">login</a>
+    <div id="menu-btn"  class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="image">
+        <img src="image/logo2.jpg" alt="no image" style="width:50%">
+    </div>
+
+    <div class="content">
+        <h3>Sistem Aduan Dan Laporan Unit Terpadu</h3>
+        <p>DisnakerTrans Kabupaten Bolaang Mongondow Utara</p>
+        <a href="#" class="btn"> Perlindungan Dan Pengawasan Tenaga Kerja <span class="fas fa-chevron-right"></span> </a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- icons section starts  -->
+
+<section class="icons-container">
+
+    <!-- <div class="icons">
+        <i class="fas fa-user-md"></i>
+        <h3>Data</h3>
+        <p></p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-users"></i>
+        <h3>1040+</h3>
+        <p></p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-procedures"></i>
+        <h3>500+</h3>
+        <p></p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-users"></i>
+        <h3>80+</h3>
+        <p></p>
+    </div> -->
+
+</section>
+
+<!-- icons section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+    <h1 class="heading"> Petunjuk <span> Pengajuan </span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-user-plus"></i>
+            <h3>Registrasi</h3>
+            <p>Registrasi Terlebih Dahulu</p>
+            <a href="#" class="btn"> Bagi Yang Sudah Registrasi Bisa Langsung Login Ke Aplikasi <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-user"></i>
+            <h3>Lengkapi Data dan Upload Dokumen</h3>
+            <p>Silakan Isi Data Yang Masih Kosong</p>
+            <a href="#" class="btn"> Upload Data Anda<span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-user-md"></i>
+            <h3>Fitur Aduan</h3>
+            <p>Buat Aduan</p>
+            <a href="#" class="btn"> Laporkan Apabila Ada Masalah Di tempat Anda Bekerja<span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-comments"></i>
+            <h3>Tindak Lanjut Aduan Secara Realtime</h3>
+            <p>Admin Akan Segera Memberikan Tindak Lanjut</p>
+            <a href="#" class="btn"> Layanan online 24 Jam <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-university"></i>
+            <h3>Perlindungan Dan Pengawasan</h3>
+            <p>Perlindungan Dan Pengawasan Bagi Tenaga Kerja Yang Telah Terdata</p>
+            <a href="#" class="btn">Perlindungan Dan Pengawasan Tenaga Kerja Menjadi Prioritas<span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-address-card"></i>
+            <h3>Pendataan Tenaga Kerja</h3>
+            <p>Kerjasama Bersama Tenaga Kerja</p>
+            <a href="#" class="btn"> Mari Bekerjasama <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>Tentang</span> Kami </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="image/gambar2.png" alt="no image" style="width:70%;">
+        </div>
+
+        <div class="content">
+        
+            <h3>Visi</h3>
+            <p>MENINGKATKAN KUALITAS KETENAGAKERJAAN DENGAN PELATIHAN KETERAMPILAN TENAGA KERJA SERTA PENGEMBANGAN KAWASAN TRANSMIGRASI YANG MAJU DAN SEJAHTERA </p>
+            <a href="#" class="btn">Misi<span class="fas fa-chevron-right"></span> </a>
+            <p>1.	Peningkatan sumber daya manusi, dengan meningkatkan keterampilan dan produktivitas tenaga kerja</p>
+            <p>2.	Peningkatan upaya perluasan kesempatan kerja dan pelayanan penempatan tenaga kerja</p>
+            <p>3.	Peningkatan pembinaan hubungan industrial yang harmonis, dinamis dan berkeadilan</p>
+            <p>4.	Peningkatan pelayanan prima</p>
+            <p>5.	Pembuatan sertifikat hak milik atas tanah warga transmigrasi</p>
+            <p>6.	Pengembangan kawasan pemukiman transmigrasi yang memenuhi standar catur layak (pengembangan sentra produksi).</p>
+            
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- doctors section starts  -->
+
+<section class="blogs" id="doctors">
+
+    <h1 class="heading"> FIle <span>Galeri</span> </h1>
+
+     <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="image/gambar1.jpeg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> Dec. 2022 </a>
+                    <a href="#"> <i class="fas fa-user"></i>NakerTrans </a>
+                </div>
+                <h3></h3>
+                <p></p>
+                <a href="#" class="btn">DisnakerTrans Team<span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/gambar3.jpeg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i>2022 </a>
+                    <a href="#"> <i class="fas fa-user"></i> NakerTrans</a>
+                </div>
+                <h3></h3>
+                <p></p>
+                <a href="#" class="btn">Pengawasan<span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/1.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 2022 </a>
+                    <a href="#"> <i class="fas fa-user"></i> NakerTrans</a>
+                </div>
+                <h3></h3>
+                <p>Pembuatan Domain salut.disnakertrans.bolmutkab.go.id</p>
+                <a href="#" class="btn">Domain<span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- doctors section ends -->
+
+<!-- booking section starts   -->
+
+<!-- <section class="book" id="book">
+
+    <h1 class="heading"> <span>Buat</span> Registrasi </h1>    
+
+    <div class="row">
+
+        <div class="image">
+            <img src="image/email.jpg" alt="">
+        </div>
+
+        <form action="">
+            <h3>Registrasi</h3>
+            <input type="text" name="email" placeholder="No Kartu Pencaker" class="box">
+            <input type="text"  name="nohp" placeholder="Nama Pencaker" class="box">
+            <input type="password" name="password" placeholder="Password" class="box">
+            <input type="date" class="box">
+            <input type="submit" style="margin:0 auto;" name="submit" style="height:35%; width:40%;font-size:14px;"class="btn btn-primary" value="Register">
+        </form>
+
+   
+
+    </div> -->
+
+</section>
+
+<!-- booking section ends -->
+
+<!-- review section starts  -->
+
+<section class="review" id="review">
+    
+    <h1 class="heading"><span>review</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="image/mou.jpg" alt="">
+            <h3>DisnakerTrans</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text"></p>
+        </div>
+
+        <div class="box">
+            <img src="image/kadis.jpg" alt="">
+            <h3>DisnakerTrans</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text"></p>
+        </div>
+
+        <div class="box">
+            <img src="image/mou2.jpg" alt="">
+            <h3>DisnakerTrans</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text"></p>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- review section ends -->
+
+<!-- blogs section starts  -->
+
+<section class="blogs" id="blogs">
+
+    <!-- <h1 class="heading"> Tentang <span>Program</span> </h1> -->
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="image/kadis.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> Dec. 2022 </a>
+                    <a href="#"> <i class="fas fa-user"></i>NakerTrans </a>
+                </div>
+                <h3></h3>
+                <p></p>
+                <a href="#" class="btn">DisnakerTrans<span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/mou.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i>2022 </a>
+                    <a href="#"> <i class="fas fa-user"></i> NakerTrans</a>
+                </div>
+                <h3></h3>
+                <p></p>
+                <a href="#" class="btn">Kabupaten Bolaang Mongondow Utara <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/mou2.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 2022 </a>
+                    <a href="#"> <i class="fas fa-user"></i> NakerTrans</a>
+                </div>
+                <h3></h3>
+                <p></p>
+                <a href="#" class="btn">Kabupaten Bolaang Mongondow Utara<span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3></h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> home </a>
+        </div>
+
+       
+
+       
+
+    </div>
+
+    <div class="credit"> Copyright <span>DisnakerTrans</span> | 2022 </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+<script>
+    function login() {
+        window.location.href = "portal/login.php"
+    }
+
+    function pengajuan() {
+        window.location.href = "portal/signup.php"
+    }
+
+    function profil() {
+        window.location.href = "portal/profil.php"
+    }
+    </script>
+</body>
+</html>
